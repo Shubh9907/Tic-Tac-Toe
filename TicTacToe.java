62d7053cafdea27c6d2f1cd95ab418d31f5487aa
Shubh9907 <shubhamv60047@gmail.com>
2021-10-21 10:44:49 +0530

    Added Code for inputing Player Choice

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,12 +1,14 @@
 package com.bridgelabz;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToe{
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic-Tac-Toe Game");
         createBoard();
+        playerChoice();
     }
 
     public static void createBoard(){
@@ -14,6 +16,19 @@ public class TicTacToe{
         for (int i=0; i<9; i++){
             board[i] = ' ';
         }
-        System.out.println(Arrays.toString(board));
+//        System.out.println(Arrays.toString(board));
+    }
+
+    public static void playerChoice() {
+        char computer;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please choose between X and O");
+        char playerInput = sc.next().charAt(0);
+        if (playerInput=='X'){
+            computer = 'O';}
+        else
+            computer = 'X';
+
+            System.out.println("You Plays with :- " +playerInput+ " and Computer Plays with :- " +computer);
     }
 }
