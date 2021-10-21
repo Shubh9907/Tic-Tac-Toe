4430e88b109fcff73ca5cf182bededeee7298e40
Shubh9907 <shubhamv60047@gmail.com>
2021-10-21 10:34:05 +0530

    Added code for creating the Game Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+
 public class TicTacToe{
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic-Tac-Toe Game");
+        createBoard();
+    }
+
+    public static void createBoard(){
+        char[] board = new char[9];
+        for (int i=0; i<9; i++){
+            board[i] = ' ';
+        }
+        System.out.println(Arrays.toString(board));
     }
 }
