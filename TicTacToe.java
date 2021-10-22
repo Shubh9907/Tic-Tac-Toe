3f5e67ef26d43005b30e4e5ccc5f65c0751232d6
Shubh9907 <shubhamv60047@gmail.com>
2021-10-22 21:49:03 +0530

    Added code to find winner or match tie

--- TicTacToe.java
+++ TicTacToe.java
@@ -10,6 +10,10 @@ public class TicTacToe {
     static char[] board = new char[9];
     static char playerCharacter;
     static char computerCharacter;
+    static String win, lose;
+    static double toss = Math.floor(Math.random() * 10 % 2);
+    static int blankSpaces;
+    static int flag = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic-Tac-Toe Game");
@@ -17,6 +21,7 @@ public class TicTacToe {
         playerChoice();
         showBoard();
         firstTurn();
+        gameFlow();
     }
 
     public static void createBoard() {
@@ -33,9 +38,13 @@ public class TicTacToe {
 
         if (playerCharacter == 'X') {
             computerCharacter = 'O';
+            win = "XXX";
+            lose = "OOO";
             System.out.println("You Plays with :- " + playerCharacter + " and Computer Plays with :- " + computerCharacter);
         } else if (playerCharacter == 'O') {
             computerCharacter = 'X';
+            win = "OOO";
+            lose = "XXX";
             System.out.println("You Plays with :- " + playerCharacter + " and Computer Plays with :- " + computerCharacter);
         } else {
             System.out.println("Please Enter a valid input (i.e. X or O)");
@@ -55,10 +64,9 @@ public class TicTacToe {
     }
 
     public static void playerMove() {
-        System.out.println("Please Choose a location where you want to put " + playerCharacter);
+        System.out.println("Your turn Please Choose a location where you want to put " + playerCharacter);
         Scanner sc = new Scanner(System.in);
         String chooseLocation = sc.next();
-        System.out.println("You Choose:- " + chooseLocation + " block");
 
         switch (chooseLocation) {
             case "A1", "a1": {
@@ -148,31 +156,85 @@ public class TicTacToe {
         showBoard();
     }
 
-    public static void computerMove(){
-        double random = Math.floor((Math.random()*100%9));
+    public static void computerMove() {
+        double random = Math.floor((Math.random() * 100 % 9));
         double random1;
-        System.out.println("random computer move "+(int)random);
-        if (board[(int)random]==' '){
-            board[(int)random]=computerCharacter;
-            random1=random;
+        if (board[(int) random] == ' ') {
+            board[(int) random] = computerCharacter;
+            random1 = random;
         } else {
-            random1=10;
-            computerMove();}
-        if (random==random1){
-        showBoard();}
+            random1 = 10;
+            computerMove();
+        }
+        if (random == random1) {
+            showBoard();
+        }
     }
 
-    public static void firstTurn(){
-        double random = Math.floor(Math.random()*10%2);
-        System.out.println("toss "+random);
-        if (random==1){
+    public static void firstTurn() {
+        if (toss == 1) {
             System.out.println("You won the toss so First turn is yours");
             playerMove();
-            computerMove();}
-        else {
+            computerMove();
+        } else {
             System.out.println("Computer won the toss so Computer has the first turn");
             computerMove();
-            playerMove();}
+            playerMove();
+        }
+    }
+
+    public static void checkWinLoseOrTie() {
+
+        String a1 = "" + board[0] + board[1] + board[2];
+        String a2 = "" + board[3] + board[4] + board[5];
+        String a3 = "" + board[6] + board[7] + board[8];
+        String a4 = "" + board[0] + board[3] + board[6];
+        String a5 = "" + board[1] + board[4] + board[7];
+        String a6 = "" + board[2] + board[5] + board[8];
+        String a7 = "" + board[0] + board[4] + board[8];
+        String a8 = "" + board[2] + board[4] + board[6];
+
+        if (a1.equals(win) || a2.equals(win) || a3.equals(win) || a4.equals(win) || a5.equals(win) || a6.equals(win) || a7.equals(win) || a8.equals(win)) {
+            System.out.println("You won the Tic-Tac-Toe Game");
+            flag=1;
+        }
+        else if (a1.equals(lose) || a2.equals(lose) || a3.equals(lose) || a4.equals(lose) || a5.equals(lose) || a6.equals(lose) || a7.equals(lose) || a8.equals(lose)) {
+            System.out.println("You Lose the Tic-Tac-Toe Game");
+            flag=1;
+        }
+        else if (blankSpaces <= 0) {
+            System.out.println("Game Draw");
+            flag=1;
+        }
+    }
+
+    public static void gameFlow() {
+        blankSpaces=7;
+        while (blankSpaces>0) {
+            if (toss == 1) {
+                if (flag==1){
+                    break;}
+                playerMove();
+                blankSpaces--;
+                checkWinLoseOrTie();
+                if (flag==1){
+                    break;}
+                computerMove();
+                blankSpaces--;
+                checkWinLoseOrTie();}
+            else {
+                if (flag==1){
+                    break;}
+                computerMove();
+                blankSpaces--;
+                checkWinLoseOrTie();
+                if (flag==1){
+                    break;}
+                playerMove();
+                blankSpaces--;
+                checkWinLoseOrTie();}
+        }
     }
 }
+
 
